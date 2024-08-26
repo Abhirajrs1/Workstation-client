6be712067db85d7c034d4b3e29f5f2d635d02383
Abhirajrs1 <132868953+Abhirajrs1@users.noreply.github.com>
2024-08-26 23:06:34 +0530

    26/8

--- src/Containers/Company/CompanyProfile/CompanyProfileForm.jsx
+++ src/Containers/Company/CompanyProfile/CompanyProfileForm.jsx
@@ -1,11 +1,200 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react';
+import { Form, Button, Card, Row, Col, InputGroup } from 'react-bootstrap';
+import Swal from 'sweetalert2';
+import CompanyNavigation from '../../../Components/CompanyNavigation';
+import './CompanyProfileForm.css'
+import axiosInstance from '../../../Services/Interceptor/companyInterceptor.js';
+import { useNavigate } from 'react-router-dom';
+import { CompanyAuth } from '../../../Context/CompanyContext';
+
 
 function CompanyProfileForm() {
+    
+    const { company, loading, Authenticated}=useContext(CompanyAuth)
+    const navigate=useNavigate()
+    const [formData, setFormData] = useState({
+        companyName: '',
+        contactNumber: '',
+        establishedYear: '',
+        companyaddress: {
+          Buildingname: '',
+          area: '',
+          street: '',
+          pincode: '',
+          city: '',
+          state: '',
+          country: 'India'
+        }
+      });
+
+      useEffect(() => {
+        if (!Authenticated && !loading) {
+          navigate('/company-login');
+        } else if (company) {
+          setFormData({
+            companyName: company.companyName || '',
+            contactNumber: company.contactNumber || '',
+            establishedYear: company.establishedYear || '',
+            companyaddress: company.companyaddress.length > 0 ? company.companyaddress[0] : {
+              Buildingname: '',
+              area: '',
+              street: '',
+              pincode: '',
+              city: '',
+              state: '',
+              country: 'India'
+            }
+          });
+        }
+      }, [company,Authenticated, navigate]);
+    
+      const handleChange = (e) => {
+        const { id, value } = e.target;
+        if (id === 'companyName' || id === 'contactNumber' || id === 'establishedYear') {
+          setFormData({
+            ...formData, [id]: value
+          });
+        } else {
+          setFormData({
+            ...formData,
+            companyaddress: {
+              ...formData.companyaddress,
+              [id]: value,
+            }
+          });
+        }
+      };
+
   return (
-    <div>
-      <h1>FORM</h1>
-    </div>
-  )
+    <>
+      <CompanyNavigation/>
+      <div className="company-contact-container">
+        <Row className="justify-content-center">
+          <Col lg={6}>
+            <Card>
+              <Card.Body>
+                <i
+                  className="fas fa-arrow-left fa-lg mb-3 company-back-icon"
+                  onClick={() => navigate('/company-profile')}
+                ></i>
+                <h5 className="card-title text-center">Company Contact Information</h5>
+                <Form onSubmit={handleSubmit}>
+                  <Form.Group className="mb-3">
+                    <Form.Label htmlFor="companyName">Company Name</Form.Label>
+                    <Form.Control
+                      type="text"
+                      id="companyName"
+                      value={formData.companyName}
+                      onChange={handleChange}
+                    />
+                  </Form.Group>
+
+                  <Form.Group className="mb-3">
+                    <Form.Label htmlFor="contactNumber">Contact Number</Form.Label>
+                    <InputGroup>
+                      <InputGroup.Text>+91</InputGroup.Text>
+                      <Form.Control
+                        type="text"
+                        id="contactNumber"
+                        value={formData.contactNumber}
+                        onChange={handleChange}
+                      />
+                    </InputGroup>
+                  </Form.Group>
+
+                  <Form.Group className="mb-3">
+                    <Form.Label htmlFor="establishedYear">Established Year</Form.Label>
+                    <Form.Control
+                      type="text"
+                      id="establishedYear"
+                      value={formData.establishedYear}
+                      onChange={handleChange}
+                    />
+                  </Form.Group>
+
+                  <Form.Group className="mb-3">
+                    <Form.Label htmlFor="Buildingname">Building Name</Form.Label>
+                    <Form.Control
+                      type="text"
+                      id="Buildingname"
+                      value={formData.companyaddress.Buildingname}
+                      onChange={handleChange}
+                    />
+                  </Form.Group>
+
+                  <Form.Group className="mb-3">
+                    <Form.Label htmlFor="area">Area</Form.Label>
+                    <Form.Control
+                      type="text"
+                      id="area"
+                      value={formData.companyaddress.area}
+                      onChange={handleChange}
+                    />
+                  </Form.Group>
+
+                  <Form.Group className="mb-3">
+                    <Form.Label htmlFor="street">Street</Form.Label>
+                    <Form.Control
+                      type="text"
+                      id="street"
+                      value={formData.companyaddress.street}
+                      onChange={handleChange}
+                    />
+                  </Form.Group>
+
+                  <Form.Group className="mb-3">
+                    <Form.Label htmlFor="city">City</Form.Label>
+                    <Form.Control
+                      type="text"
+                      id="city"
+                      value={formData.companyaddress.city}
+                      onChange={handleChange}
+                    />
+                  </Form.Group>
+
+                  <Form.Group className="mb-3">
+                    <Form.Label htmlFor="state">State</Form.Label>
+                    <Form.Control
+                      type="text"
+                      id="state"
+                      value={formData.companyaddress.state}
+                      onChange={handleChange}
+                    />
+                  </Form.Group>
+
+                  <Form.Group className="mb-3">
+                    <Form.Label htmlFor="pincode">Pincode</Form.Label>
+                    <Form.Control
+                      type="text"
+                      id="pincode"
+                      value={formData.companyaddress.pincode}
+                      onChange={handleChange}
+                    />
+                  </Form.Group>
+
+                  <Form.Group className="mb-3">
+                    <Form.Label htmlFor="country">Country</Form.Label>
+                    <Form.Control
+                      type="text"
+                      id="country"
+                      value={formData.companyaddress.country}
+                      onChange={handleChange}
+                    />
+                  </Form.Group>
+
+                  <div className="d-flex justify-content-center mt-4">
+                    <Button type="submit" className="company-contact-btn">
+                      Save
+                    </Button>
+                  </div>
+                </Form>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    </>
+  );
 }
 
 export default CompanyProfileForm
