6fcc84613c1913c8fc5c4d7cf6850cc4753523fe
Abhirajrs1 <132868953+Abhirajrs1@users.noreply.github.com>
2024-09-07 10:29:38 +0530

    Footer updated

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,37 +1,47 @@
-import React from 'react'
-
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import './Footer.css';
 
 function Footer() {
   return (
-    <div className='bg-dark'>
-    {/* <div className='container'> */}
-        <div className='row py-5 text-white'>
-      <div className='col-md-3 col-sm-12'>
-        <h4 className='text-warning'>Company Name</h4>
-        <p>WORK STATION</p>
+    <footer className="footer">
+      <Container>
+        <Row className="py-4">
+          <Col md={3} sm={6} className="mb-3">
+            <h5 className="footer-heading">Company Name</h5>
+            <p>WORK STATION</p>
+          </Col>
+          <Col md={3} sm={6} className="mb-3">
+            <h5 className="footer-heading">Services</h5>
+            <ul className="footer-list">
+              <li>About</li>
+              <li>Contact Us</li>
+            </ul>
+          </Col>
+          <Col md={3} sm={6} className="mb-3">
+            <h5 className="footer-heading">Useful Links</h5>
+            <ul className="footer-list">
+              <li>Shipping</li>
+            </ul>
+          </Col>
+          <Col md={3} sm={6} className="mb-3">
+            <h5 className="footer-heading">Address</h5>
+            <p>WorkStation,Palarivattom,Kochi-3</p>
+            <p>PH:9874563210</p>
+          </Col>
+        </Row>
+      </Container>
+      <div className="copyright">
+        <Container>
+          <Row>
+            <Col className="text-center py-3">
+              <p className="mb-0">&copy; Copyright WorkStation 2024</p>
+            </Col>
+          </Row>
+        </Container>
       </div>
-      <div className='col-md-3 col-sm-12'>
-        <h4 className='text-warning'>Services</h4>
-        <p>About</p>
-        <p>Countact Us</p>
-        <p>Post a job</p>
-      </div>
-      <div className='col-md-3 col-sm-12'>
-         <h4  className='text-warning'>Useful Links</h4>
-         <p>Shipping</p>
-      </div>
-      <div className='col-md-3 col-sm-12'>
-        <h4  className='text-warning'>Address</h4>
-        <p>WorkStation,Palarivattom,Kochi-3</p>
-        <p>PH:9874563210</p>
-      </div>
-   </div>
-   <div className="copyright-container d-flex justify-content-center align-items-center pb-3">
-  <p className="text-white">© Copyright WorkStation 2024</p>
-</div>    
-</div>
-    // </div>
-  )
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
